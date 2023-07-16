02145ae29da94a2e7875d039470034a59f318e4c
Berin Aniesh <berinaniesh@gmail.com>
2023-07-16 09:53:53 +0530

    separate module for models

---
+++ src/models/hello.rs
@@ -0,0 +1,22 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct HelloMessage {
+	pub greeting: String,
+	pub about: String,
+	pub authentication: String,
+	pub framework: String,
+	pub database: String,
+}
+
+impl Default for HelloMessage {
+    fn default() -> HelloMessage {
+		HelloMessage{
+			greeting: String::from("Hello there!"),
+			about: String::from("Backend for TNCars"),
+			authentication: String::from("JWT"),
+			framework: String::from("Actix-Web"),
+			database: String::from("Postgresql"),
+		}
+	}
+}
--- src/routes/hello.rs
+++ src/routes/hello.rs
@@ -1,26 +1,5 @@
-use serde::{Serialize, Deserialize};
 use actix_web::{get, HttpResponse};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct HelloMessage {
-	greeting: String,
-	about: String,
-	authentication: String,
-	framework: String,
-	database: String,
-}
-
-impl Default for HelloMessage {
-	fn default() -> HelloMessage {
-		HelloMessage{
-			greeting: String::from("Hello there!"),
-			about: String::from("Backend for TNCars"),
-			authentication: String::from("JWT"),
-			framework: String::from("Actix-Web"),
-			database: String::from("Postgresql"),
-		}
-	}
-}
+use crate::models::hello::HelloMessage;
 
 #[get("/")]
 pub async fn hello() -> HttpResponse {
