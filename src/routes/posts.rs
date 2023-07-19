2c051fd87a907179fcd3f2b71b8d409a830f8a0a
Berin Aniesh <berinaniesh@gmail.com>
2023-07-19 17:34:27 +0530

    update

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -7,7 +7,7 @@ use actix_web::{post, web, HttpRequest, HttpResponse};
 pub async fn create_post(
     req: HttpRequest,
     app_state: web::Data<AppState>,
-    fixed_form: web::Json<CreatePost>,
+    mut form: web::Json<CreatePost>,
 ) -> HttpResponse {
     let user_id_result = get_id_from_request(&req);
     if user_id_result.is_err() {
@@ -18,25 +18,25 @@ pub async fn create_post(
     let user_id = user_id_result.unwrap();
     //let fixed_form = fix_createpost_form(&form);
     let query = sqlx::query(
-        "INSERT INTO posts (title, user_id, brand, price, model_year, km_driven, transmission, fuel, description, location) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)") 
-        .bind(&fixed_form.title)
-        .bind(user_id) 
-        .bind(&fixed_form.brand)
-        .bind(&fixed_form.price) 
-        .bind(&fixed_form.model_year)
-        .bind(&fixed_form.km_driven) 
-        .bind(&fixed_form.transmission)
-        .bind(&fixed_form.fuel)
-        .bind(&fixed_form.description)
-        .bind(&fixed_form.location)
+        "INSERT INTO posts (title, user_id, brand, price, model_year, km_driven, transmission, fuel, description, location) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)")
+        .bind(&form.title)
+        .bind(user_id)
+        .bind(&form.brand)
+        .bind(&form.price)
+        .bind(&form.model_year)
+        .bind(&form.km_driven)
+        .bind(&form.transmission)
+        .bind(&form.fuel)
+        .bind(&form.description)
+        .bind(&form.location)
         .execute(&app_state.pool)
         .await;
+
     if query.is_ok() {
         return HttpResponse::Created().json(Response {
             message: "Post created successfully".to_string(),
         });
     }
-    dbg!(query);
     return HttpResponse::InternalServerError().json(Response {
         message: "Something went wrong, try again later".to_string(),
     });
