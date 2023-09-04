da5fbc0a398fb5b056ea72359a70f686d3fd947f
Berin Aniesh <berinaniesh@gmail.com>
2023-09-04 14:20:51 +0530

    update .env.example

--- src/misc/appstate.rs
+++ src/misc/appstate.rs
@@ -1,4 +1,3 @@
-use dotenvy::var;
 use sqlx::postgres::{PgPool, PgPoolOptions};
 
 #[derive(Clone)]
@@ -6,21 +5,8 @@ pub struct AppState {
     pub pool: PgPool,
 }
 
-fn get_database_url() -> String {
-    let db_host = var("DB_HOST").unwrap();
-    let db_port = var("DB_PORT").unwrap();
-    let db_name = var("DB_NAME").unwrap();
-    let db_username = var("DB_USERNAME").unwrap();
-    let db_password = var("DB_PASSWORD").unwrap();
-    let database_url = format!(
-        "postgresql://{}:{}@{}:{}/{}",
-        db_username, db_password, db_host, db_port, db_name
-    );
-    return database_url;
-}
-
 pub async fn get_appstate() -> AppState {
-    let database_url: String = get_database_url();
+    let database_url: String = dotenvy::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool: PgPool = PgPoolOptions::new()
         .max_connections(5)
         .connect(database_url.as_str())
